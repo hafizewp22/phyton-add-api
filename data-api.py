57393c73712b5bafedf1b158c146ab30fa690ebb
Hafiz E. Wedo Putra <52981264+hafizewp22@users.noreply.github.com>
2023-11-16 18:29:58 +0700

    Update data-api.py

--- data-api.py
+++ data-api.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = '...'
 db = SQLAlchemy(app)
 
 class Customer(db.Model):
